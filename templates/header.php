6aaa05c52fe302348e5b5c5c6d7606ed9c12c2fc
Mason L <mason.lawlor@me.com>
2015-05-20 17:41:17 -0600

    Revert cc2591d..18fb6e8
    This rolls back to commit cc2591d5b98c5c202ca3f9f74e99a985cca66302.

--- templates/header.php
+++ templates/header.php
@@ -1,34 +1,12 @@
-<?php use Roots\Sage\Nav\NavWalker; ?>
-
-<header class="banner navbar navbar-default navbar-static-top" role="banner">
+<header class="banner" role="banner">
   <div class="container">
-    <nav class="collapse navbar-collapse" role="navigation">
-      <div class="row">
-        <div class="col-xs-12">
-          <ul class="primary-site-nav-sign-in">
-            <li class="nav-item"><a class="demo-link" href="#">Store</a></li>
-          </ul>
-          <nav role="navigation">
-            <?php
-            if (has_nav_menu('primary_navigation')) :
-              wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-            endif;
-            ?>
-          </nav>
-        </div>
-      </div>
-    </div>
-
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+    <nav role="navigation">
+      <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+      endif;
+      ?>
     </nav>
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
-
-
   </div>
 </header>
--- templates/home-hero.php
+++
@@ -1,9 +0,0 @@
-<div class="video-wrap">
-  <div class="mobile-bg">
-    <video autoplay loop id="bgvid">
-      <source src="polina.webm" type="video/webm">
-      <source src="<?php echo get_stylesheet_directory_uri(); ?>/assets/video/web-video-loop.mp4" type="video/mp4">
-    </video>
-    <svg class="logo"><?php get_template_part( 'assets/svg/inline', 'logo-light-grey.svg' ); ?></svg>
-  </div>
-</div>
